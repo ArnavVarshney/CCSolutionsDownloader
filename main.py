93ec0897cc1649871e16ddba4ba0120bad8e5a87
Arnav Varshney <varshney.arnav@gmail.com>
2019-12-10 17:14:34 +0530

    Everything should somewhat work
    Signed-off-by: ArnavVarshney <varshney.arnav@gmail.com>

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
+import os
+
 import lxml.html as lx
 import requests
 from bs4 import BeautifulSoup as bs
 
-username = ''
-password = ''
+username = input('CodeChef Username: ')
+
+try:
+    os.mkdir('CodeChef Solutions')
+except FileExistsError:
+    pass
+
+try:
+    os.mkdir(str('CodeChef Solutions/' + username))
+except FileExistsError:
+    pass
+
+password = input('CodeChef Password: ')
 problems = {}
+
+# Missing: PIKE,
+extensions = {'ADA': '.abd', 'C++14': '.cpp', 'JAVA': '.java', 'PYTH': '.py', 'PYTH 3.6': '.py', 'C#': '.cs',
+              'PAS fpc': '.pas', 'PAS gpc': '.pas', 'RUBY': '.ruby', 'PHP': '.php', 'GO': '.go', 'NODEJS': '.js',
+              'HASK': '.hs', 'SCALA': 'scala', 'D': '.d', 'PERL': '.pl', 'FORT': '.f', 'WSPC': '.ws', 'CAML': '.ml',
+              'BF': '.bf', 'ASM': '.asm', 'CLPS': '.cli', 'PRLG': '.pro', 'ICON': '.icn', 'SCM qobi': '.scm',
+              'ST': '.st', 'NICE': '.nice', 'LUA': '.lua', 'BASH': '.sh', 'NEM': '.n', 'LISP sbcl': '.lisp',
+              'LISP clisp': '.lisp', 'SCM guile': '.scm', 'JS': '.js', 'PYPY3': '.py'}
+headers = {
+    'user-agent': 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/77.0.3865.120 Safari/537.36 OPR/64.0.3417.92'
+}
 
 with requests.session() as s:
     def main():
-        # Ask for input and get user's page
-        global username, password
-        username = input('CodeChef Username: ')
-        password = input('CodeChef Password: ')
         # Login to https://www.codechef.com (Credits: https://brennan.io/2016/03/02/logging-in-with-requests/)
         try:
-            login = s.get('https://www.codechef.com/')
+            login = s.get('https://www.codechef.com/', headers=headers)
             login_html = lx.fromstring(login.text)
             hidden_inputs = login_html.xpath(r'//form//input[@type="hidden"]')
             form = {x.attrib["name"]: x.attrib["value"] for x in hidden_inputs}
@@ -23,45 +44,78 @@ with requests.session() as s:
             response = s.post('https://www.codechef.com/', data=form)
             if response.url == 'https://www.codechef.com/node':
                 print('Logged in!')
+                get_problems()
             elif response.url == 'https://www.codechef.com/session/limit':
                 print('Session limit reached! Logout of all active sessions before continuing!')
             else:
-                print('Login failed!')
+                print('Login failed! Check credentials and try again!')
+            s.get('https://www.codechef.com/logout', headers=headers)
         except:
             print('Some error occurred!')
+            s.get('https://www.codechef.com/logout', headers=headers)
 
 
-    def get_problems():
-        len_username = len(username)
-        r = requests.get('https://www.codechef.com/users/' + username)
-        data = r.text
-        soup = bs(data, 'html5lib')
-        # Classify all problems according to their contests
-        for link in soup.find_all('a'):
-            x = str(link.get('href'))
-            if 'status' in x and username in x:
-                x = x[:len(x) - len_username - 1]
-                ls = x.split('/')
-                if len(ls) == 3:
-                    contest_code = 'PRACTICE'
-                    problem_code = str(ls[2])
-                else:
-                    contest_code = str(ls[1])
-                    problem_code = str(ls[3])
-                if contest_code not in problems.keys():
-                    problems[contest_code] = []
-                problems[contest_code].append(problem_code)
-        # for i in problems:
-        #    print(str(i) + ': ' + ''.join([str(j) + ', ' for j in problems[i]]))
+def get_problems():
+    print('Fetching AC problems!')
+    len_username = len(username)
+    r = s.get('https://www.codechef.com/users/' + username, headers=headers)
+    data = r.text
+    soup = bs(data, 'html5lib')
+    # Classify all problems according to their contests
+    for link in soup.find_all('a'):
+        x = str(link.get('href'))
+        if 'status' in x and username in x:
+            x = x[:len(x) - len_username - 1]
+            ls = x.split('/')
+            if len(ls) == 3:
+                contest_code = 'PRACTICE'
+                problem_code = str(ls[2])
+            else:
+                contest_code = str(ls[1])
+                problem_code = str(ls[3])
+            if contest_code not in problems.keys():
+                problems[contest_code] = []
+            problems[contest_code].append(problem_code)
+    extract_solutions()
 
 
-    def extract_solutions():
-        # Extract solutions
-        for i in problems:
-            for j in problems[i]:
-                r = requests.get('https://www.codechef.com/' + i + '/status/' + j + ',' + username)
-                data = r.text
-                soup = bs(data, 'html5lib')
+def extract_solutions():
+    # Extract solutions
+    print('Extracting solutions!')
+    for i in problems:
+        try:
+            os.mkdir(str('CodeChef Solutions/' + username + '/' + i))
+        except:
+            print('Some error occurred during creation of folders!')
+        for j in problems[i]:
+            if i == 'PRACTICE':
+                r = s.get(
+                    'https://www.codechef.com/status/' + j + ',' + username + '?sort_by=All&sorting_order=asc&language=All&status=15&Submit=GO',
+                    headers=headers)
+            else:
+                r = s.get(
+                    'https://www.codechef.com/' + i + '/status/' + j + ',' + username + '?sort_by=All&sorting_order=asc&language=All&status=15&Submit=GO',
+                    headers=headers)
+            data = r.text
+            soup = bs(data, 'html5lib')
+            ls = soup.findAll('td', {'width': '60'})
+            sol_id = ls[0].text
+            r = s.get('http://www.codechef.com/viewsolution/' + sol_id, headers=headers)
+            data = r.text
+            soup = bs(data, 'html5lib')
+            st = str(soup)
+            lang_code = st[st.index('languageShortName') + 20:st.index('solutionMemory') - 3]
+            r = s.get('http://www.codechef.com/viewplaintext/' + sol_id, headers=headers)
+            data = r.text
+            soup = bs(data, 'html5lib')
+            code = soup.findAll('pre')[0].text
+            filename = j + ' ' + sol_id + extensions[lang_code]
+            path = str('CodeChef Solutions/' + username + '/' + i + '/' + filename)
+            code_file = open(path, 'w+')
+            code_file.write(code)
+            code_file.close()
+            print(i + ' ' + filename + ' saved')
+
 
 if __name__ == "__main__":
     main()
